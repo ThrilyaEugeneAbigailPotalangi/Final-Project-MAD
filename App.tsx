758d5c9167c61b7d624bb04822555a22bc28f03f
ThrilyaEugeneAbigailPotalangi <S22310548@student.unklab.ac.id>
2025-06-26 04:08:30 +0800

    push3

--- App.tsx
+++ App.tsx
@@ -1,10 +1,19 @@
 import React from 'react';
-import SignIn from './src/pageProject/SignIn';
-import SplashScreen from './src/pageProject/SplashScreen';
-import SignUp from './src/page/SignUp'
+import SplashScreen from './Final-Project-MAD/src/pageProject/SplashScreen';
+import SignIn from './Final-Project-MAD/src/pageProject/SignIn';
+import SignUp from './Final-Project-MAD/src/pageProject/SignUp';
+import ForgetPassword from './Final-Project-MAD/src/pageProject/ForgetPassword';
+import OnBoarding1 from './Final-Project-MAD/src/pageProject/OnBoarding1';
+import OnBoarding2 from './Final-Project-MAD/src/pageProject/OnBoarding2';
+import Dashboard from './Final-Project-MAD/src/pageProject/Dashboard';
+import CreateTask from './Final-Project-MAD/src/pageProject/CreateTask';
+import Expenses from './Final-Project-MAD/src/pageProject/Expenses';
+import Goals from './Final-Project-MAD/src/pageProject/Goals';
+import Reminder from './Final-Project-MAD/src/pageProject/Reminder';
+import Profile from './Final-Project-MAD/src/pageProject/Profile';
 
 const App = () => {
-  return <SignIn />;
+  return <Profile/>;
 };
 
 export default App;
--- index.js
+++ index.js
@@ -8,4 +8,5 @@ import App from './App';
 
 import {name as appName} from './app.json';
 
+
 AppRegistry.registerComponent(appName, () => App);
